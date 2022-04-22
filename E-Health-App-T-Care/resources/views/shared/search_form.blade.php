a0e731570a5f3d4cb050432e522dfff2e100cce7
KOBeerose <tahaelghabi@gmail.com>
2022-04-22 15:48:26 +0000

    Notebook changes

--- E-Health-App-T-Care/resources/views/shared/search_form.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="search_form">
-  <h2>Trouvez un médecin</h2>
-  <form action="/medecins">
-    <input type="text" name="s" class="form-control" placeholder="Par example: adam generaliste rabat" value="{{ app('request')->input('s') }}">
-    <button type="submit" class="btn btn-primary">Rechercher</button>
-  </form>
-</div>
--- E-Health-App-T-Care/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-question'></i> Users</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('booking') }}'><i class='nav-icon la la-question'></i> Bookings</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('consultation') }}'><i class='nav-icon la la-question'></i> Consultations</a></li>
